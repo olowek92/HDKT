26efc4fbf14a07923f7a0b884a3c281e216e54cd
Wojciech <olowek92@gmail.com>
2022-10-02 23:31:36 +0200

    added 2 photos

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./TeamStyles.css";
-import TomekZ from "../assets/persons/TomekZ.JPEG";
 import AnetaEdi from "../assets/persons/Aneta&Edi.jpeg";
+import RenataJacek from "../assets/persons/Renata&Jacek.jpg";
+import TomekZ from "../assets/persons/TomekZ.JPEG";
+import TomekCufe from "../assets/persons/Tomek-Cufe.jpg";
+
 
 const Team = () => {
   const anetaEdi = "Aneta&Edi";
+  const renataJacek = "Renata&Jacek";
 
   return (
     <div className="team">
@@ -14,23 +18,21 @@ const Team = () => {
           <p>- {anetaEdi} -</p>
           <p>- RoadGlide -</p>
         </div>
+        <div className="card card1">
+          <img className="image" src={RenataJacek} alt={renataJacek} />
+          <p>- {renataJacek} -</p>
+          <p>- SuperLow -</p>
+        </div>
         <div className="card">
           <img className="image" src={TomekZ} alt="Tomek" />
           <p>- Tomek -</p>
           <p>- Breakout -</p>
         </div>
-        {/* <div className="card"> */}
-        {/* <p className="btc">PHOTO</p> */}
-          {/* <img className="image" src='' alt="" />
-          <p>-  -</p>
-          <p>-  -</p> */}
-        {/* </div> */}
-        {/* <div className="card"> */}
-          {/* <p className="btc">PHOTO</p> */}
-          {/* <img className="image" src="" alt="" />
-          <p>-  -</p>
-          <p>-  -</p> */}
-        {/* </div> */}
+        <div className="card card1">
+          <img className="image" src={TomekCufe} alt="Tomek" />
+          <p>- Tomek -</p>
+          <p>- Iron 883 -</p>
+        </div>
       </div>
     </div>
   );
